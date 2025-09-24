c20dd259cfa0eceda9cb1897e39040f1e54976ab
Taufi <kd@klausdresbach.de>
2025-09-24 14:29:49 +0200

    Initial Commit

---
+++ ASCookBook/ASCookBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASCookBookApp.swift
+//  ASCookBook
+//
+//  Created by Klaus Dresbach on 24.09.25.
+//
+
+import SwiftUI
+
+@main
+struct ASCookBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
