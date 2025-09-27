95814dceb6ceba1297aaa5f8a63f76f0bb32e3dd
Taufi <kd@klausdresbach.de>
2025-09-27 17:38:12 +0200

    Finish first part DB integration. Import attributes and to-one-relations

--- ASCookBook/Models/Season.swift
+++ ASCookBook/Models/Season.swift
@@ -4,4 +4,14 @@
 //
 //  Created by Klaus Dresbach on 24.09.25.
 //
+import SwiftData
 
+@Model
+final class Season {
+    var title: String
+    @Relationship(inverse: \Recipe.season) var recipes: [Recipe] = []
+    
+    init(title: String) {
+        self.title = title
+    }
+}
