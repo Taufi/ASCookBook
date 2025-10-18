b366551acdd9f29ce39f4712dcea982b56cd3396
Taufi <kd@klausdresbach.de>
2025-10-18 15:15:48 +0200

    add camera picker as uiview controller representable

---
+++ ASCookBook/Helpers/CameraPicker.swift
@@ -0,0 +1,49 @@
+//
+//  CameraPicker.swift
+//  ASCookBook
+//
+//  Created by Klaus Dresbach on 28.09.25.
+//
+
+import SwiftUI
+import UIKit
+
+struct CameraPicker: UIViewControllerRepresentable {
+    @Binding var selectedImageData: Data?
+    @Environment(\.dismiss) private var dismiss
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = .camera
+        picker.allowsEditing = true
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: CameraPicker
+        
+        init(_ parent: CameraPicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let editedImage = info[.editedImage] as? UIImage {
+                parent.selectedImageData = editedImage.jpegData(compressionQuality: 0.8)
+            } else if let originalImage = info[.originalImage] as? UIImage {
+                parent.selectedImageData = originalImage.jpegData(compressionQuality: 0.8)
+            }
+            parent.dismiss()
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.dismiss()
+        }
+    }
+}
